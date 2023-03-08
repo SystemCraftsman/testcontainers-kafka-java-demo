3a9947f11e190f84acd753d21b16b550e3e89974
Aykut M. Bulgu <aykut@systemcraftsman.com>
2023-03-09 01:12:34 +0300

    Update AppTest.java

--- apps/earthquake-notifier/src/test/java/com/systemcraftsman/demo/AppTest.java
+++ apps/earthquake-notifier/src/test/java/com/systemcraftsman/demo/AppTest.java
@@ -1,19 +1,13 @@
 package com.systemcraftsman.demo;
 
-import com.github.javafaker.Faker;
 import com.systemcraftsman.demo.deserializer.NotificationDeserializer;
 import com.systemcraftsman.demo.model.LocationNotification;
 import com.systemcraftsman.demo.model.Notification;
-import com.systemcraftsman.demo.serializer.NotificationSerializer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.Test;
 import org.rnorth.ducttape.unreliables.Unreliables;
 import org.testcontainers.containers.KafkaContainer;
@@ -24,10 +18,8 @@ import org.testcontainers.utility.DockerImageName;
 
 import java.time.Duration;
 import java.util.Collections;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 //TODO: Annotate with @Testcontainers
